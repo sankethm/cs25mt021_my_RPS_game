d857e6dfe626b25195232d4ea122f61069f7bd48
hmsanket <hmsanket.926@gmail.com>
2025-08-24 23:20:17 +0530

    explaining the rules of the game

--- RPS.c
+++ RPS.c
@@ -4,4 +4,12 @@ int main(){
     printf("***********************************************\n");
     printf("******Welcome to Rock Paper Scissor game ******\n");
     printf("***********************************************\n");
+
+    printf("**************************************Rules of the Game ***************************************\n");
+    printf("*                     two players secretly pick one of rock, paper, or scissors.              *\n");
+    printf("*  Both players reveal their selection to the other player at once; the winner is chosen      *\n");
+    printf("*  based on what the selections are. Rock beats scissors (by crushing them); scissors         *\n");
+    printf("*  beats paper (by cutting it); and paper beats rock (by covering it). If both players select *\n");
+    printf("*                                the same one, it is a tie.                                   *\n");
+    printf("***********************************************************************************************\n");
 }
