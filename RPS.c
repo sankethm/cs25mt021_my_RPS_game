20b4bb2f97a61e4c6ef69b3987ad60077675236b
hmsanket <hmsanket.926@gmail.com>
2025-08-24 23:10:20 +0530

    greeting the user

---
+++ RPS.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(){
+    printf("***********************************************\n");
+    printf("******Welcome to Rock Paper Scissor game ******\n");
+    printf("***********************************************\n");
+}
